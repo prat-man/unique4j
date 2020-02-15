416fad474ed0f6cbeb5b591dad5ddd023dccd331
Pratanu Mandal <pratanu.mandal@gmail.com>
2020-02-16 01:22:48 +0530

    Removed Unique4jException constructor to maintain compatibility with Java 1.6
    Removed Unique4jException constructor to maintain compatibility with Java 1.6

--- src/main/java/tk/pratanumandal/unique4j/exception/Unique4jException.java
+++ src/main/java/tk/pratanumandal/unique4j/exception/Unique4jException.java
@@ -32,10 +32,6 @@ public class Unique4jException extends Exception {
 		super();
 	}
 
-	public Unique4jException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
-		super(arg0, arg1, arg2, arg3);
-	}
-
 	public Unique4jException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
 	}

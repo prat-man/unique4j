6911024c7740cc8fc507bf47e79a90466b5c21d6
Pratanu Mandal <pratanu.mandal@gmail.com>
2020-02-16 19:56:37 +0530

    Added test cases for checking newlines and carriage returns escaping

--- src/test/java/tk/pratanumandal/unique4j/Unique4jTest.java
+++ src/test/java/tk/pratanumandal/unique4j/Unique4jTest.java
@@ -273,6 +273,142 @@ public class Unique4jTest {
 	}
 	
 	@Test
+	public void testUnique4jNewline1() throws Unique4jException {
+		
+		final Object lock = new Object();
+		
+		final List<String> received = new ArrayList<String>();
+		
+		final String message = "hello\nworld";
+		
+		Unique4j unique1 = new Unique4j(APP_ID, false) {
+			@Override
+			protected String sendMessage() {
+				// send null
+				return null;
+			}
+			
+			@Override
+			protected void receiveMessage(String arg0) {
+				// to assert on main thread
+				received.add(arg0);
+				
+				// notify that message has been received
+				synchronized (lock) {
+					lock.notify();
+				}
+			}
+		};
+		
+		// try to obtain lock
+		unique1.acquireLock();
+		
+		Unique4j unique2 = new Unique4j(APP_ID, false) {
+			@Override
+			protected String sendMessage() {
+				// send message
+				return message;
+			}
+			
+			@Override
+			protected void receiveMessage(String arg0) {
+				// do nothing
+			}
+		};
+		
+		// try to obtain lock
+		unique2.acquireLock();
+		
+		// wait until message is received
+		if (received.isEmpty()) {
+			synchronized (lock) {
+				try {
+					lock.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		// assert if message is sent correctly
+		Assert.assertEquals(received.get(0), message);
+		
+		// try to free the locks before exiting program
+		unique1.freeLock();
+		
+		unique2.freeLock();
+		
+	}
+	
+	@Test
+	public void testUnique4jNewline2() throws Unique4jException {
+		
+		final Object lock = new Object();
+		
+		final List<String> received = new ArrayList<String>();
+		
+		final String message = "hello\r\nworld";
+		
+		Unique4j unique1 = new Unique4j(APP_ID, false) {
+			@Override
+			protected String sendMessage() {
+				// send null
+				return null;
+			}
+			
+			@Override
+			protected void receiveMessage(String arg0) {
+				// to assert on main thread
+				received.add(arg0);
+				
+				// notify that message has been received
+				synchronized (lock) {
+					lock.notify();
+				}
+			}
+		};
+		
+		// try to obtain lock
+		unique1.acquireLock();
+		
+		Unique4j unique2 = new Unique4j(APP_ID, false) {
+			@Override
+			protected String sendMessage() {
+				// send message
+				return message;
+			}
+			
+			@Override
+			protected void receiveMessage(String arg0) {
+				// do nothing
+			}
+		};
+		
+		// try to obtain lock
+		unique2.acquireLock();
+		
+		// wait until message is received
+		if (received.isEmpty()) {
+			synchronized (lock) {
+				try {
+					lock.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		// assert if message is sent correctly
+		Assert.assertEquals(received.get(0), message);
+		
+		// try to free the locks before exiting program
+		unique1.freeLock();
+		
+		unique2.freeLock();
+		
+	}
+	
+	@Test
 	public void testUnique4jList() throws Unique4jException {
 		
 		final Object lock = new Object();

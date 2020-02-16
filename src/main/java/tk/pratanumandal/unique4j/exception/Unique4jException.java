159d4cfbc621d6e590717ae04ea8cf2d19038119
Pratanu Mandal <pratanu.mandal@gmail.com>
2020-02-16 21:14:39 +0530

    Added JavaDoc for exception class

--- src/main/java/tk/pratanumandal/unique4j/exception/Unique4jException.java
+++ src/main/java/tk/pratanumandal/unique4j/exception/Unique4jException.java
@@ -28,20 +28,42 @@ public class Unique4jException extends Exception {
 	
 	private static final long serialVersionUID = 268060627071973613L;
 
+	/**
+	 * Constructs a new exception with null as its detail message.
+	 */
 	public Unique4jException() {
 		super();
 	}
-
-	public Unique4jException(String arg0, Throwable arg1) {
-		super(arg0, arg1);
+	
+	/**
+	 * Constructs a new exception with the specified detail message.<br>
+	 * The cause is not initialized, and may subsequently be initialized by a call to {@link #initCause}.
+	 * 
+	 * @param message the detail message. The detail message is saved for later retrieval by the {@link #getMessage()} method.
+	 */
+	public Unique4jException(String message) {
+		super(message);
+	}
+	
+	/**
+	 * Constructs a new exception with the specified detail message and cause.<br><br>
+	 * Note that the detail message associated with cause is not automatically incorporated in this exception's detail message.
+	 * 
+	 * @param message the detail message (which is saved for later retrieval by the {@link #getMessage()} method).
+	 * @param cause the cause (which is saved for later retrieval by the {@link #getCause()} method). (A null value is permitted, and indicates that the cause is nonexistent or unknown.)
+	 */
+	public Unique4jException(String message, Throwable cause) {
+		super(message, cause);
 	}
 
-	public Unique4jException(String arg0) {
-		super(arg0);
-	}
-
-	public Unique4jException(Throwable arg0) {
-		super(arg0);
+	/**
+	 * Constructs a new exception with the specified cause and a detail message of (cause==null ? null : cause.toString()) (which typically contains the class and detail message of cause).<br>
+	 * This constructor is useful for exceptions that are little more than wrappers for other throwables (for example, {@link java.security.PrivilegedActionException}).
+	 * 
+	 * @param cause the cause (which is saved for later retrieval by the {@link #getCause()} method). (A null value is permitted, and indicates that the cause is nonexistent or unknown.)
+	 */
+	public Unique4jException(Throwable cause) {
+		super(cause);
 	}
 
 }

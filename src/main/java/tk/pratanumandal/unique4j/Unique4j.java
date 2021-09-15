470cff9e6cb5c747159e2b143946a625da36fb19
Pratanu Mandal <pratanu.mandal@gmail.com>
2021-09-16 03:41:13 +0530

    Removed unused import

--- src/main/java/tk/pratanumandal/unique4j/Unique4j.java
+++ src/main/java/tk/pratanumandal/unique4j/Unique4j.java
@@ -35,7 +35,6 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 
